159aec96804eaa168c4a56abd86272693c8b7ba7
rabbymohiuddin <rabbymohiuddin@gmail.com>
2020-09-24 15:47:05 +0600

    weather value chage accord to city implemented

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,25 +6,49 @@ import Chart from "../Chart/Chart";
 const Home = () => {
   const [basicInfo, setBasicInfo] = useState({});
   const [weather, setWeather] = useState([]);
+  const [city, setCity] = useState("dhaka");
+  const [text, setText] = useState("");
   useEffect(() => {
     Axios.get(
-      "https://api.openweathermap.org/data/2.5/weather?q=dhaka&units=metric&APPID=10615dc32d56f42782f5a50a04f91f30"
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=10615dc32d56f42782f5a50a04f91f30`
     ).then((data) => {
       // console.log(data.data);
       setBasicInfo(data.data.main);
       setWeather(data.data.weather);
     });
-  }, []);
+  }, [city]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setCity(text);
+    setText("");
+  };
+  const onChange = (e) => {
+    e.preventDefault(setText(e.target.value));
+  };
   return (
     <div id="home">
       <div className="dark-overlay">
         <div className="container">
-          <form>
-            <input type="text" className="form-group" placeholder="city name" />
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              className="form-group"
+              value={text}
+              onChange={onChange}
+              placeholder="city name"
+            />
           </form>
+          <p style={{ color: "#0099ff" }} className="text-center">
+            Search by any city name of any country of the world. Make sure you
+            spell correct.
+          </p>
           <div className="row py-5">
             <div className="col-md-6">
-              <Result result={basicInfo} weather={weather}></Result>
+              <Result
+                result={basicInfo}
+                weather={weather}
+                cityName={city}
+              ></Result>
             </div>
             <div className="col-md-6">
               <Chart></Chart>
